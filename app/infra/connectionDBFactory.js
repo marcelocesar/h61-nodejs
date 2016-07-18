dd362b5c9cd298533d798f08bd6cd3e11191bfce
Marcelo Cesar <marcelop3@gmail.com>
2016-07-17 22:06:31 -0300

    ajustes deploy

--- app/infra/connectionDBFactory.js
+++ app/infra/connectionDBFactory.js
@@ -21,6 +21,15 @@ function createDBConnection() {
             database: 'casadocodigo_nodejs_test'
         });
     }
+    
+    if (process.env.NODE_ENV === 'production'){
+        return mysql.createConnection({
+            host: 'us-cdbr-iron-east-04.cleardb.net',
+            user: 'b3c44b8226d131',
+            password: 'cec4b433',
+            database: 'heroku_25b2a8ffc9280c3'
+        });
+    }
 }
 
 //Pattern Wrapper
